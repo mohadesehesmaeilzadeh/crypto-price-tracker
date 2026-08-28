d7950088958da23c122be1639d9b05552a148c73
mohadesehesmaeilzadeh <mohadesehesmaeilzadeh712@gmail.com>
2026-08-28 22:44:16 +0330

    remove: clean CRA starter files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app">
+      <h1>Cryptocurrency Price Tracker</h1>
+
+      <p>
+        Live cryptocurrency prices
+      </p>
+    </main>
   );
 }
 
